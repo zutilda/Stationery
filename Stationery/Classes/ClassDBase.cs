eaff65b4a668c24c2b2197ac2e9eecd6745ebd77
Настя Жулина <julina.nastia2017@yandex.ru>
2023-03-04 16:41:46 +0300

    Добавьте файлы проекта.

---
+++ Stationery/Classes/ClassDBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stationery
+{
+    internal class ClassDBase
+    {
+        public static Entities DB;
+    }
+}
---
+++ Stationery/Classes/ClassFrame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Stationery
+{
+    internal class ClassFrame
+    {
+        public static Frame headFrame;
+        public static Frame basicFrame;         
+    }
+}
