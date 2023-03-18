ad68c6a8d2a49f33a2281aaf25e2284ecc77a9d4
Настя Жулина <julina.nastia2017@yandex.ru>
2023-03-18 15:27:22 +0300

    captcha

--- Stationery/Page/Authorization.xaml.cs
+++ Stationery/Page/Authorization.xaml.cs
@@ -148,6 +148,7 @@ namespace Stationery
                 {
                     Entrance.IsEnabled = true;
                     CheckEntrance.Visibility=Visibility.Collapsed;
+                    Captha.Text = "";
                 }
                 else
                 {
